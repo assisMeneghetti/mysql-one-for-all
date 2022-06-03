1fb127c846e98dd0397e619d962e02811398cdd7
assisMeneghetti <assismfabri@hotmail.com>
2022-06-03 13:46:53 -0300

    feat: show albums by a specific artist

---
+++ desafio8.sql
@@ -0,0 +1,8 @@
+SELECT 
+    a.artista, al.album
+FROM
+    SpotifyClone.artistas AS a
+        INNER JOIN
+    SpotifyClone.albuns AS al ON a.artista_id = al.artista_id
+WHERE
+    a.artista = 'Walter Phoenix';
